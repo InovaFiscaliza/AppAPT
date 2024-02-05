a60433fd8827136e15cc6dcdb64f2697204cdab9
Cleyton Agapito <cragapito@gmail.com>
2024-02-05 19:04:59 -0300

    Novo layout do winAPTmain

--- +Analysers/CONSTANTS.m
+++ +Analysers/CONSTANTS.m
@@ -4,5 +4,8 @@ classdef CONSTANTS
         % Para teste de conexão, usar esse timeout.
         % A ser removido em favor de uma Config.
         CONNTIMEOUT = 10;
+
+
+        VERSION = '0.37';
     end
 end
