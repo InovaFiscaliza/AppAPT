f0ec4ad22aaefc40c15634d06c5ca1348c959501
Cleyton Agapito <cragapito@gmail.com>
2024-03-06 18:15:35 -0300

    Testes de implementação do modelo

--- +control/CTX.m
+++ +control/CTX.m
@@ -1,10 +1,15 @@
-classdef winAPTmain
+classdef CTX < handle
 
     properties
+        setup
         context
     end
     
     methods
+        function obj = CTX(~)
+            disp('INFO: *** Controlador de contexto iniciado ***');          
+        end
+
         function app = winAPTmain(~)
             % Reutiliza o appColetaV2 se ativo
             % TODO: Deve rodar com outras versões além do R2023a
@@ -24,6 +29,18 @@ classdef winAPTmain
             [instrHandle, ~] = apt.utils.getInstrumentHandler(app, idx);
             Instr = Analysers.Analyser.instance(rawIDN);
             Instr.conn = instrHandle;
+        end
+
+        function changed(obj)
+            disp('Context:')
+            obj.setup.CW.value
+            obj.setup.CW.color
+
+            if isfield(obj.setup.CW, 'message')
+                disp('O campo "message" existe na estrutura obj.setup.CW.');
+            else
+                disp('O campo "message" não existe na estrutura obj.setup.CW.');
+            end
         end
     end
 end
